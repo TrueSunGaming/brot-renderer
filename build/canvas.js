0433ccf24001db6f2c20d69b0011aed33312325d
TrueSunGaming <79117622+TrueSunGaming@users.noreply.github.com>
2022-08-12 16:02:53 -0400

    remove build

--- build/canvas.js
+++
@@ -1,4 +0,0 @@
-var _a, _b;
-const canvas = (_a = document.querySelector("canvas")) !== null && _a !== void 0 ? _a : document.createElement("canvas");
-const ctx = (_b = canvas === null || canvas === void 0 ? void 0 : canvas.getContext("2d")) !== null && _b !== void 0 ? _b : new CanvasRenderingContext2D;
-export { canvas, ctx };
